6cb960b9b4b6f867b312cc883e6528fa29597efe
Thr0TT1e <7hr0771e@gmail.com>
2022-11-28 02:14:17 +0300

    Настроен шаблон для 1024

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import * as bootstrapIcons from 'bootstrap-icons/font/bootstrap-icons.css';
-import * as bootstrap from 'bootstrap';
 import '../sass/main.sass';
+import * as bootstrap from 'bootstrap';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import ymaps from 'ymaps';
 
 document.addEventListener('DOMContentLoaded', function () {
   // header
@@ -54,4 +57,69 @@ document.addEventListener('DOMContentLoaded', function () {
     .addEventListener('click', function () {
       openSearchHandler();
     });
+
+  const swiperProjects = new Swiper('.projects__swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: false,
+    navigation: {
+      nextEl: '.projects__next',
+      prevEl: '.projects__prev',
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 33.97,
+        slidesPerGroup: 2,
+      },
+      1008: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+        slidesPerGroup: 2,
+      },
+      1750: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        slidesPerGroup: 3,
+      },
+    },
+  });
+
+  ymaps.load().then((maps) => {
+    const map = new maps.Map('loadMap', {
+      center: [55.760109, 37.591952],
+      zoom: 14,
+      controls: [],
+    });
+
+    const placemark = new maps.Placemark(
+      [55.760109, 37.591952],
+      {},
+      {
+        iconLayout: 'default#image',
+        iconImageHref: 'image/map/placemark.svg',
+        iconImageSize: [20, 20],
+        iconImageOffset: [-2, -40],
+      }
+    );
+
+    map.controls
+      .add('zoomControl', {
+        float: 'none',
+        position: {
+          right: 11,
+          top: 300,
+        },
+      })
+      .add('geolocationControl', {
+        float: 'none',
+        position: {
+          right: 11,
+          top: 380,
+        },
+      });
+
+    map.geoObjects.add(placemark);
+    map.behaviors.disable('scrollZoom');
+  });
 });
