51781e9eda9d7cd2305a8e832a9c509bdd482e62
Thr0TT1e <7hr0771e@gmail.com>
2023-01-16 00:41:10 +0300

    Правка сборки, динамическая favicon

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,19 @@ export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
   resolve: {
     alias: {
